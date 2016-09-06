056a9c2fe9f46d7e75454d9011807215d7fbc2fa
Marsel Kadrov <marselkadroff@gmail.com>
2016-09-06 19:34:48 +0500

    первый коммит

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>Блог Кадрова Марселя</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+<body>
+  <div class="main-container">
+    <form enctype="multipart/form-data" method="post" action="post.php">
+
+      <input type="text" class="center" name="title" placeholder="Введите заголовок"></input>
+      <input type="text" class="center" name="content" placeholder="Введите текст"></input>
+      <input type="submit" class="center" name="submit" value="отправить"></input>
+
+    </form>
+
+  </div>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,5 @@
+<?php
+  require 'database/database_connection.php';
+
+
+ ?>
